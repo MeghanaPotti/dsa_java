19626bf79676ca5f91a25e2ab4967d2b1afdcfe8
Meghana Potti <pottimeghana123@gmail.com>
2024-01-03 22:38:15 +0530

    Arrays

--- src/dsa_arrays/MinMaxNumber.java
+++ src/dsa_arrays/MinMaxNumber.java
@@ -1,10 +1,31 @@
 package dsa_arrays;
 
 public class MinMaxNumber {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	    int[] numbers = {1,2,3};
+	    int min=numbers[0];
+	    int max=numbers[0];
+	   
+	   void minmax() {
+	   
+	   for(int i=0;i<numbers.length;++i) {
+			
+			if (numbers[i]<min) {  
+				min = numbers[i];
+			}
+			 if (numbers[i]>max) {
+				max = numbers[i];
+			}
+		}
+	   }
+	   
+	   public static void main (String[] args) {
+		
+		   MinMaxNumber object = new 	MinMaxNumber();
+		   object.minmax();
+		   System.out.println("Minimum number is "+object.min);
+		   System.out.println("Maximum number is "+object.max);   
+	   }
 }
+	
+
--- src/dsa_arrays/SortArray.java
+++ src/dsa_arrays/SortArray.java
@@ -2,8 +2,26 @@ package dsa_arrays;
 
 public class SortArray {
 
+	int[] numbers = {21,11,31,51,41};
+	int min = numbers[0];
+	
+	void sorting() {
+	
+	for (int i=0; i<numbers.length ; ++i) {
+		 
+		if (numbers[i]<min) {
+			min = numbers[i];
+			numbers[i]++;
+		}
+		
+	}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SortArray object = new SortArray();
+		object.sorting();
+		System.out.println("Order of array is "+object.min);
 
 	}
 
