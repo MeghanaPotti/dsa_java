494bc7590ac7253bb76d39a9d4681b1f15075337
Meghana Potti <pottimeghana123@gmail.com>
2024-01-05 23:01:41 +0530

    Occurences

---
+++ src/dsa_arrays/NoOfOccurence.java
@@ -0,0 +1,25 @@
+package dsa_arrays;
+
+public class NoOfOccurence {
+	
+	
+	public static void main(String[] args) {
+		
+		char name[] = {'M','e','g','h','a','n','a'};
+		  
+		char find='m';
+		
+		int count=0;
+		
+		for(int i=0;i<name.length;i++) {
+			
+			if(name[i]==find) {
+				count++;
+			}
+			
+		}
+		System.out.println("Count of "+find+" is " +count);
+
+	}
+
+}
