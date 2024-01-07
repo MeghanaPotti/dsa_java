35c6d8d58cad9d133f5a2902272a078d3d1a461a
Meghana Potti <pottimeghana123@gmail.com>
2024-01-07 23:31:29 +0530

    lowercase alphabets

---
+++ src/dsa_strings/PrintingLowercases.java
@@ -0,0 +1,13 @@
+package dsa_strings;
+
+public class PrintingLowercases {
+
+	public static void main(String[] args) {
+		
+			 char ch;
+		      for(ch = 'a'; ch <= 'z'; ++ch)
+		      System.out.print(ch + " ");
+
+	}
+
+}
