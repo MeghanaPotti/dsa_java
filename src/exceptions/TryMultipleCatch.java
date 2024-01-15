7f5aca8cd0162e8806dcf933c9ce9b3be92fb74f
MeghanaPotti <pottimeghana123@gmail.com>
2024-01-15 23:02:59 +0530

    try with multiple catch

---
+++ src/exceptions/TryMultipleCatch.java
@@ -0,0 +1,24 @@
+package exceptions;
+
+public class TryMultipleCatch {
+
+	public static void main(String[] args) {
+		
+		 try{    
+	           
+				int array[] = new int[10];    
+	            array[10] = 30/0;    
+	        }    
+	        catch(ArithmeticException e){  
+	            System.out.println(e);  
+	        }    
+	        catch(ArrayIndexOutOfBoundsException e){  
+	            System.out.println(e);  
+	        }    
+	        catch(Exception e){  
+	            System.out.println(e);  
+	        }    
+
+	}
+
+}
